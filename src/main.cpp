c04696c4b1051932135ad6b21fae3057e3cef8f2
claudio-korb <claudioakorb@gmail.com>
2023-08-24 09:33:48 -0300

    [OPENGL] First vertex attribute

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,9 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
     glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
 
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
+    
     while(!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
