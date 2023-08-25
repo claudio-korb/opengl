40e3c7a7c73d9628788f8d32e497d6040fbf0edb
claudio-korb <claudioakorb@gmail.com>
2023-08-25 09:12:48 -0300

    [OPENGL] First vertex and fragment shaders (incode)

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,47 @@
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = &source[0];
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int compile_result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &compile_result);
+    if(compile_result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "Vertex" : "Fragment") << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader); 
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 static float pxToCoordinateX(double x)
 {
     return (2*x - WINDOW_WIDTH) / (WINDOW_WIDTH);
@@ -60,6 +101,29 @@ int main()
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
     
+    std::string vertex_shader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragment_shader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertex_shader, fragment_shader);
+    glUseProgram(shader);
+
     while(!glfwWindowShouldClose(window))
     {
         static uint8_t canClick = true;
@@ -75,6 +139,7 @@ int main()
 
         glfwPollEvents();
 
+        //New Triangle ------------------------
         int click = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT); 
 
         if(canClick && click == GLFW_PRESS)
@@ -102,6 +167,9 @@ int main()
             canClick = true;
         }
     }
+
+    glDeleteProgram(shader);
+    glfwTerminate();
 
     return 0;
 }
