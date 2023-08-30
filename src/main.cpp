111d79484c50d1817c43fd08185a9e234e493f5a
claudio-korb <claudioakorb@gmail.com>
2023-08-30 20:02:01 -0300

    [OPENGL] Abstracted vertex arrays into its class

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "renderer.h"
 #include "vertexbuffer.h"
 #include "indexbuffer.h"
+#include "vertexarray.h"
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
@@ -61,13 +62,13 @@ int main()
 
     unsigned int vao;
 
-    GLCall(glGenVertexArrays(1, &vao));
-    GLCall(glBindVertexArray(vao));
-
+    VertexArray va;
     VertexBuffer vb(positions, 4 * 2 * sizeof(float));
-    GLCall(glEnableVertexAttribArray(0));
-    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0));
-    
+
+    VertexBufferLayout layout;
+    layout.Push<float>(2);
+    va.AddBuffer(vb, layout);
+        
     IndexBuffer ib(indices, 6);
 
     Shader shader = Shader("../res/shaders/Basic.glsl");
@@ -100,7 +101,7 @@ int main()
             GLCall(glUniform4f(uniform_location, r, 0.3f, 0.3f, 1.0f));
         }
 
-        GLCall(glBindVertexArray(vao));
+        va.Bind();
         ib.Bind();
 
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
