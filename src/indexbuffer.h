8b364fe7a27067dac89f17c881a6bdd46619dd11
claudio-korb <claudioakorb@gmail.com>
2023-08-30 13:22:50 -0300

    [OPENGL] Abstracted index buffer and vertex buffer into classes

--- src/indexbuffer.h
+++ src/indexbuffer.h
@@ -1,14 +1,17 @@
 #pragma once
 
-class VertexBuffer
+class IndexBuffer
 {
 private:
     unsigned int m_RendererID;
+    unsigned int m_Count;
 
 public:
-    VertexBuffer(const void* data, unsigned int size);
-    ~VertexBuffer();
+    IndexBuffer(const unsigned int* data, unsigned int count);
+    ~IndexBuffer();
 
-    void Bind();
-    void Unbind();
-}
+    void Bind() const;
+    void Unbind() const;
+
+    inline unsigned int GetCount() const {return m_Count ;}
+};
