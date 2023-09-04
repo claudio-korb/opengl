76d762668be1a0ff9b27b944f4b6a3eea3d046bb
claudio-korb <claudioakorb@gmail.com>
2023-09-04 20:05:06 -0300

    [OPENGL] Created ortho matrix projection

--- src/shader.cpp
+++ src/shader.cpp
@@ -103,17 +103,6 @@ void Shader::Unbind() const
     GLCall(glUseProgram(0));
 }
 
-
-void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
-{
-    GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
-}
-
-void Shader::SetUniform1i(const std::string& name, int v0)
-{
-    GLCall(glUniform1i(GetUniformLocation(name), v0));
-}
-
 unsigned int Shader::GetUniformLocation(const std::string& name)
 {
     if(m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
@@ -128,4 +117,19 @@ unsigned int Shader::GetUniformLocation(const std::string& name)
     
     m_UniformLocationCache[name] = location;
     return location;
+}
+
+void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
+{
+    GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
+}
+
+void Shader::SetUniformMat4f(const std::string& name, const glm::mat4& matrix)
+{
+    GLCall(glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0]));
+}
+
+void Shader::SetUniform1i(const std::string& name, int v0)
+{
+    GLCall(glUniform1i(GetUniformLocation(name), v0));
 }
